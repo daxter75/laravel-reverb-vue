ca82efc7f78b518c6d690e7a75ab39418fe2a6c3
daxter75 <daxter75@gmail.com>
2024-07-03 15:02:35 +0200

    protected route only for logged users

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::post('/messages', function() {
-    $payload = request()->validate([
-        'message' => 'required',
-        'id' => 'required'
-    ]);
-    MessageReceived::dispatch($payload['message'], $payload['id']);
-    return response()->json(['message' => 'Message received!']);
-});
 
-Route::get('/visit-count', function() {
-    return Inertia::render("VisitCount");
-});
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -38,6 +27,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::post('/messages', function() {
+        $payload = request()->validate([
+            'message' => 'required',
+            'id' => 'required'
+        ]);
+        MessageReceived::dispatch($payload['message'], $payload['id']);
+        return response()->json(['message' => 'Message received!']);
+    });
+    
+    Route::get('/visit-count', function() {
+        return Inertia::render("VisitCount");
+    });
 });
 
 require __DIR__.'/auth.php';
