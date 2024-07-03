434f3f206a7d997db5d97ccfce669f059735b4d7
daxter75 <daxter75@gmail.com>
2024-07-03 14:43:45 +0200

    exchange messages

--- resources/js/Pages/VisitCount.vue
+++ resources/js/Pages/VisitCount.vue
@@ -1,13 +1,37 @@
 <script setup>
 import { ref } from 'vue';
 
-const count = ref(0);
+const messages = ref([]);
+const message = ref("");
 
-Echo.channel("hello-world").listen("HelloWorld", (e) => {
-    count.value++;
-});
+Echo.private("messages").listen("MessageReceived", (e) => {
+  if(!messages.value.find((m) => m.id === e.id)) {
+    messages.value.push(e);
+  }  
+})
+
+function handleSubmit() {
+  const msg = {
+    id: crypto.randomUUID(),
+    message: message.value
+  }
+  messages.value.push({
+    ...msg,
+    who: "Me",
+  });
+  axios.post("/messages", msg);
+}
+
 </script>
 
 <template>
-  <div>The visit count is {{ count }}</div>
+  <ul>
+    <li v-for="message in messages" :key="message.id">
+      {{ message.message }} - {{ message.who }}
+    </li>
+  </ul>
+  <form @submit.prevent="handleSubmit">
+    <textarea v-model="message" cols="30" rows="10"></textarea>
+    <button>Send Message</button>
+  </form>
 </template>
