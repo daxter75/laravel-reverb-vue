8173ac969b5f7fd4138c97b7f522ae97ae157a78
daxter75 <daxter75@gmail.com>
2024-07-03 12:05:29 +0200

    implemented counter with vuejs

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,23 @@
 <?php
 
+use App\Events\HelloWorld;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
+    HelloWorld::dispatch();
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
+});
+
+Route::get('/visit-count', function() {
+    return Inertia::render("VisitCount");
 });
 
 Route::get('/dashboard', function () {
